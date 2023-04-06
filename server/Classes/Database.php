14cf2be7a011a6d92efc6e2b0534acfa7af2bc61
LukaOrjonikidze <99496792+LukaOrjonikidze@users.noreply.github.com>
2023-04-06 23:19:23 +0400

    Update Database.php

--- server/Classes/Database.php
+++ server/Classes/Database.php
@@ -17,6 +17,7 @@ class Database implements IDatabase {
     }
     public function connect(){
         $this->conn = mysqli_connect($this->host, $this->user, $this->password, $this->db_name);
+        mysqli_set_charset($this->conn, "utf8mb4");
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
